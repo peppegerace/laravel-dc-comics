6cc14f2952d83c8dcd7f12723c062453c798ed38
peppegerace <peppegerace47@gmail.com>
2023-11-23 18:47:47 +0100

    [add] migrations & seeders, populated the database

---
+++ database/migrations/2023_11_23_173035_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 100)->unique();
+            $table->text('description')->nullable();
+            $table->text('thumb');
+            $table->string('price', 10);
+            $table->string('series', 50)->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
